81b4b7c9a42290a3b75cf1f451b429299824c32d
mc1128 <mc1128@naver.com>
2021-05-07 16:07:54 +0900

    emailjs를 이용한 메일 보내기 기능 추가

--- keyboard.js
+++ keyboard.js
@@ -383,14 +383,24 @@ $('form').submit(function() {
       return false;
     }
   });
-  
+
+  var Template_Form = {	
+    //emailJS에서 설정한 템플릿과 동일한 명으로 작성
+          name: $('input[name=user_name]').val(),
+          subject : $('select[name=subject]').val(),
+          message : $('textarea[name=message]').val(),
+          email: $('input[name=user_email]').val(), 
+  };
+
   //Form에 에러가 없다면 성공창 background색상을 바꾸고 창을 다시 닫는다
   if (!formError) {
+    emailjs.send('service_8lwonwz', 'template_d3ovd0e', Template_Form);
     $('body').addClass('form-submitted');
     $('#form-head').addClass('form-submitted'); 
     setTimeout(function(){
         $(form).trigger("reset");
         toggleForm();
+        bindFormClick();
     }, 1300);
   }
   return false;
@@ -400,4 +410,9 @@ $('form').submit(function() {
 function isValidEmail(email) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(email);
+};
+
+// emailjs init 설정
+window.onload = function(){
+    emailjs.init("user_mteO3t9ifOULEgm3T1bzj");
 };
